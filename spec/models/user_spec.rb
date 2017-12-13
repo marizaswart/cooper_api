2698cf9027b09e9c1b346eee35eb109f370a463d
marizaswart <mariza.swart@gmail.com>
2017-12-13 15:46:42 +0100

    Add new namespace to skip omniaut_callback in routes, test the endpoints

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe User, type: :model do
+RSpec.describe User, type: :model do
   it 'should have valid Factory' do
     expect(FactoryBot.create(:user)).to be_valid
   end
