45e45498150ac3b4123f44564536db543d146264
marizaswart <mariza.swart@gmail.com>
2017-12-15 09:22:22 +0100

    Config app

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module CooperApi
@@ -20,8 +16,10 @@ module CooperApi
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete]
+        origins ‘*’
+        resource ‘*’, headers: :any, methods: [:get, :post, :put, :delete],
+                 expose: %w(access-token expiry token-type uid client),
+                 max_age: 0
       end
     end
   end
