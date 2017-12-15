9819177031c3d0bf402c8ae5e9255b1ad76ecdc3
marizaswart <mariza.swart@gmail.com>
2017-12-15 10:34:37 +0100

    Fix quotation error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-     namespace :v0 do
-       resources :ping, only: [:index], contraints: { format: 'json' }
-     end
+    namespace :v0 do
+      resources :ping, only: [:index], contraints: { format: 'json' }
+    end
 
     namespace :v1, defaults: { format: :json } do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
